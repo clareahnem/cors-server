2386b6a104c4d1a50c185bb0a85009f9fadbc942
ahnemui <clh.oneil@gmail.com>
2021-10-26 09:35:55 +1100

    ahh try again

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://nastydealer.netlify.app'],
-    requireHeader: ['https://clare-cors-server.herokuapp.com/', 'x-requested-with'],
+    originWhitelist: ['http://localhost:3000', 'https://nastydealer.netlify.app', 'https://evilinsult.com/generate_insult.php?lang=en&type=json'],
+    requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
