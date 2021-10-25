a47718060a07f0e965288af54d0f37e6e1adbf76
ahnemui <clh.oneil@gmail.com>
2021-10-26 09:29:21 +1100

    changed origin for test

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://clare-cors-server.herokuapp.com/', 'https://nastydealer.netlify.app'],
-    requireHeader: ['origin', 'x-requested-with'],
+    originWhitelist: ['http://localhost:3000', 'https://nastydealer.netlify.app'],
+    requireHeader: ['https://clare-cors-server.herokuapp.com/', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
